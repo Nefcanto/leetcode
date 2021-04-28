547c012a3576c1af405c95cae77f3c944948b484
Nefcanto <sn.1361@gmail.com>
2021-04-28 19:42:48 +0430

    finding longest common prefix

---
+++ src/Host/LongestCommonPrefix.cs
@@ -0,0 +1,29 @@
+﻿public class LongestCommonPrefix
+{
+    public string Find(string[] inputs)
+    {
+        var index = 0;
+        var longestCommonPrefix = "";
+        while (true)
+        {
+            if (inputs[0].Length < index + 1)
+            {
+                return longestCommonPrefix;
+            }
+            var lastChar = inputs[0][index];
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                if (inputs[i].Length < index + 1)
+                {
+                    return longestCommonPrefix;
+                }
+                if (inputs[i][index] != lastChar)
+                {
+                    return longestCommonPrefix;
+                }
+            }
+            longestCommonPrefix += lastChar;
+            index++;
+        }
+    }
+}
--- src/Host/Program.cs
+++ src/Host/Program.cs
@@ -6,7 +6,7 @@ namespace Host
     {
         static void Main(string[] args)
         {
-            var result = new RomanToInt().Convert("III");
+            var result = new Solution().LongestCommonPrefix(new string[] { "flower", "flow", "flight" });
         }
     }
 }
