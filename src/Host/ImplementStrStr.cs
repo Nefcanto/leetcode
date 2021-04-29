cb599440b46043a2657745f45db621b8b1e72e45
Nefcanto <sn.1361@gmail.com>
2021-04-29 18:10:30 +0430

    implement str str

---
+++ src/Host/ImplementStrStr.cs
@@ -0,0 +1,32 @@
+﻿public class ImplementStrStr
+{
+    public int StrStr(string haystack, string needle)
+    {
+        if (needle == null || needle == "")
+        {
+            return 0;
+        }
+        for (int i = 0; i < haystack.Length; i++)
+        {
+            if (haystack[i] == needle[0])
+            {
+                for (int j = 0; j < needle.Length; j++)
+                {
+                    if (i + j >= haystack.Length)
+                    {
+                        break;
+                    }
+                    if (haystack[i + j] != needle[j])
+                    {
+                        break;
+                    }
+                    if (j == needle.Length - 1)
+                    {
+                        return i;
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+}
--- src/Host/Program.cs
+++ src/Host/Program.cs
@@ -6,8 +6,7 @@ namespace Host
     {
         static void Main(string[] args)
         {
-            var nums = new int[] { 0, 0, 1, 3, 9, 1, 5, 1, 1, 7 };
-            var result = new RemoveElement().Remove(nums, 1);
+            var result = new ImplementStrStr().StrStr("aaa", "aaaa");
         }
     }
 }
