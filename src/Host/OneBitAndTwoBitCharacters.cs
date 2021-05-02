92c4c11d21e718c99a922f8e0578f8d4f4a62d6c
Nefcanto <sn.1361@gmail.com>
2021-05-02 23:27:30 +0430

    1 bit and 2 bit character

---
+++ src/Host/OneBitAndTwoBitCharacters.cs
@@ -0,0 +1,30 @@
+﻿public class OneBitAndTwoBitCharacters
+{
+    public bool IsOneBitCharacter(int[] bits)
+    {
+        int? last = null;
+        for (int i = 0; i < bits.Length; i++)
+        {
+            if (last.HasValue)
+            {
+                if (last == 1)
+                {
+                    last = null;
+                }
+                else
+                {
+                    last = bits[i];
+                }
+            }
+            else
+            {
+                last = bits[i];
+            }
+        }
+        if (last.HasValue)
+        {
+            return true;
+        }
+        return false;
+    }
+}
--- src/Host/Program.cs
+++ src/Host/Program.cs
@@ -7,7 +7,7 @@ namespace Host
     {
         static void Main(string[] args)
         {
-            var result = new AddBinary().Add("1010", "1011");
+            var result = new OneBitAndTwoBitCharacters().IsOneBitCharacter(new int[] { 1, 0, 0 });
         }
     }
 }
