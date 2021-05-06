c9f9b2586af017155d23e9450c8be1baafbed79c
Nefcanto <sn.1361@gmail.com>
2021-05-06 16:38:03 +0430

    pascal triangle

---
+++ src/Host/PascalTriangle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class PascalTriangle
+{
+    public IList<IList<int>> Generate(int numRows)
+    {
+        var rows = new List<int[]>();
+        for (int i = 1; i <= numRows; i++)
+        {
+            var row = new int[i];
+            row[0] = 1;
+            row[i - 1] = 1;
+            for (int j = 1; j <= i - 2; j++)
+            {
+                row[j] = rows[i - 2][j - 1] + rows[i - 2][j];
+            }
+            rows.Add(row);
+        }
+        return (IList<IList<int>>)rows.ToArray();
+    }
+}
--- src/Host/Program.cs
+++ src/Host/Program.cs
@@ -7,29 +7,7 @@ namespace Host
     {
         static void Main(string[] args)
         {
-            // [5,4,8,11,null,13,4,7,2,null,null,null,1]
-            var result = new PathSum().HasPathSum(
-
-                new TreeNode(5,
-                    new TreeNode(4,
-                        new TreeNode(11,
-                            new TreeNode(7),
-                            new TreeNode(2)
-                            ),
-                        null
-                    ),
-                    new TreeNode(8,
-                            new TreeNode(13),
-                            new TreeNode(4,
-                                null,
-                                new TreeNode(1)
-                                )
-                        )
-                ),
-                22
-
-                );
-
+            var result = new PascalTriangle().Generate(5);
         }
     }
 }
