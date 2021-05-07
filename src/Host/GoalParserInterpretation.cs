e4afabdcb842344a8e90889b967e9f79b2466c1e
Nefcanto <sn.1361@gmail.com>
2021-05-07 14:50:57 +0430

    goal parser interpretation

---
+++ src/Host/GoalParserInterpretation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+public class Solution
+{
+    public string Interpret(string command)
+    {
+        var maps = new Dictionary<string, string>();
+        maps.Add("G", "G");
+        maps.Add("()", "o");
+        maps.Add("(al)", "al");
+        var token = "";
+        var index = 0;
+        var result = "";
+        while (index < command.Length)
+        {
+            token = "";
+            while (!maps.ContainsKey(token))
+            {
+                token += command[index].ToString();
+                index++;
+            }
+            result += maps[token];
+        }
+        return result;
+    }
+}
