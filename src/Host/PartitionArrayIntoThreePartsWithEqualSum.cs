d5cdf6425514ca7562c30d900576973ba7479201
Nefcanto <sn.1361@gmail.com>
2021-05-07 11:06:53 +0430

    partition array into three parts with equal sum

---
+++ src/Host/PartitionArrayIntoThreePartsWithEqualSum.cs
@@ -0,0 +1,43 @@
+﻿// slow
+public class PartitionArrayIntoThreePartsWithEqualSum
+{
+    public bool CanThreePartsEqualSum(int[] arr)
+    {
+        var first = 0;
+        var second = 0;
+        var third = 0;
+        var border = 1;
+        while (border < arr.Length)
+        {
+            first = 0;
+            second = 0;
+            third = 0;
+            for (int i = 0; i < border; i++)
+            {
+                first += arr[i];
+            }
+            for (int i = border; i < arr.Length; i++)
+            {
+                second += arr[i];
+                if (first == second)
+                {
+                    if (i == arr.Length - 1)
+                    {
+                        return false;
+                    }
+                    third = 0;
+                    for (int z = arr.Length - 1; z > i; z--)
+                    {
+                        third += arr[z];
+                    }
+                    if (second == third)
+                    {
+                        return true;
+                    }
+                }
+            }
+            border++;
+        }
+        return false;
+    }
+}
--- src/Host/Program.cs
+++ src/Host/Program.cs
@@ -11,7 +11,7 @@ namespace Host
     {
         static void Main(string[] args)
         {
-            var result = new FactorialTrailingZeros().TrailingZeroes(75);
+            var result = new PartitionArrayIntoThreePartsWithEqualSum().CanThreePartsEqualSum(new int[] { -5, 9, 7, -5, -9, 10, 8, 7, -2, 1, -3, 1, -6, -7, -5, -9, -2, -6, 2, -4, -8, -7, 1, -5, 9, 9, 3, 7, -3, -2, -3, -6, 7, -7, -4, -8, 10, 1, -9, 4, -7, 5, 7, 6, -2 });
         }
     }
 }
