cacd88482a3ed522c653ce94c8194687a7ea2065
Nefcanto <sn.1361@gmail.com>
2021-05-07 05:16:53 +0430

    check if binary string has at most one segment of ones

---
+++ src/Host/CheckIfBinaryStringHasAtMostOneSegmentOfOnes.cs
@@ -0,0 +1,28 @@
+﻿public class CheckIfBinaryStringHasAtMostOneSegmentOfOnes
+{
+    public bool CheckOnesSegment(string s)
+    {
+        var ones = 0;
+        var started = false;
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == '1')
+            {
+                if (!started)
+                {
+                    ones++;
+                }
+                started = true;
+            }
+            if (s[i] == '0')
+            {
+                started = false;
+            }
+            if (ones > 1)
+            {
+                return false;
+            }
+        }
+        return ones == 1;
+    }
+}
