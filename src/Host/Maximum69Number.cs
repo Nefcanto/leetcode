d3d5e43100e408f66b92cba742da31643800b6db
Nefcanto <sn.1361@gmail.com>
2021-05-08 22:28:28 +0430

    faster algorithm for maximum 69 number

--- src/Host/Maximum69Number.cs
+++ src/Host/Maximum69Number.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public class Maximum69Number
 {
@@ -11,5 +12,31 @@ public class Maximum69Number
             text = text.Remove(firstSix, 1).Insert(firstSix, "9");
         }
         return Convert.ToInt32(text);
+    }
+
+    public int FindFaster(int num)
+    {
+        var nums = new List<int>();
+        while (num > 0)
+        {
+            nums.Add(num % 10);
+            num = num / 10;
+        }
+        nums.Reverse();
+        for (int i = 0; i < nums.Count; i++)
+        {
+            if (nums[i] == 6)
+            {
+                nums[i] = 9;
+                break;
+            }
+        }
+        nums.Reverse();
+        var result = 0;
+        for (int i = 0; i < nums.Count; i++)
+        {
+            result += nums[i] * (int)Math.Pow(10, i);
+        }
+        return result;
     }
 }
