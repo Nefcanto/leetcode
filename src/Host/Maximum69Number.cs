f1118892e9b6425a1c3e5564c406947f335b1c3c
Nefcanto <sn.1361@gmail.com>
2021-05-08 22:16:05 +0430

    maximum 69 number

---
+++ src/Host/Maximum69Number.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Maximum69Number
+{
+    public int Find(int num)
+    {
+        var text = num.ToString();
+        var firstSix = text.IndexOf("6");
+        if (firstSix != -1)
+        {
+            text = text.Remove(firstSix, 1).Insert(firstSix, "9");
+        }
+        return Convert.ToInt32(text);
+    }
+}
