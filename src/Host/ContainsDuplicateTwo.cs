986973d6b577209de873a91290091fe56d78cf7a
Nefcanto <sn.1361@gmail.com>
2021-05-08 22:01:13 +0430

    contains dupliacate two

---
+++ src/Host/ContainsDuplicateTwo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class ContainsDuplicateTwo
+{
+    public bool ContainsNearbyDuplicate(int[] nums, int k)
+    {
+        var uniques = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (uniques.ContainsKey(nums[i]))
+            {
+                if (Math.Abs(i - uniques[nums[i]]) <= k)
+                {
+                    return true;
+                }
+                else
+                {
+                    uniques[nums[i]] = i;
+                }
+            }
+            else
+            {
+                uniques.Add(nums[i], i);
+            }
+        }
+        return false;
+    }
+}
