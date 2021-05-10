2b62c8ced44181ed8f36fdf9d3301f737a009798
Nefcanto <sn.1361@gmail.com>
2021-05-10 16:54:20 +0430

    make two arrays equal by reversing subarrays

---
+++ src/Host/MakeTwoArraysEqualByReversingSubArrays.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class MakeTwoArraysEqualByReversingSubArrays
+{
+    public bool CanBeEqual(int[] target, int[] arr)
+    {
+        Array.Sort(target);
+        Array.Sort(arr);
+        for (int i = 0; i < target.Length; i++)
+        {
+            if (target[i] != arr[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
