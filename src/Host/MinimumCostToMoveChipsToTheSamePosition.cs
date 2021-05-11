9e2155a956b721af0582af6d7106a3220a28c66d
Nefcanto <sn.1361@gmail.com>
2021-05-11 14:21:23 +0430

    minimum cost to move chips to the same position (not passing all the tests)

---
+++ src/Host/MinimumCostToMoveChipsToTheSamePosition.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+// not passing all the tests
+public class MinimumCostToMoveChipsToTheSamePosition
+{
+    public int MinCostToMoveChips(int[] position)
+    {
+        var dic = new Dictionary<int, int>();
+        var max = 0;
+        var maxIndex = 0;
+        for (int i = 0; i < position.Length; i++)
+        {
+            if (dic.ContainsKey(position[i]))
+            {
+                dic[position[i]]++;
+            }
+            else
+            {
+                dic.Add(position[i], 1);
+            }
+            if (dic[position[i]] > max)
+            {
+                max = dic[position[i]];
+                maxIndex = position[i];
+            }
+        }
+        var cost = 0;
+        for (int i = 0; i < position.Length; i++)
+        {
+            if (Math.Abs(position[i] - maxIndex) % 2 != 0)
+            {
+                cost++;
+            }
+        }
+        return cost;
+    }
+}
--- src/Host/Program.cs
+++ src/Host/Program.cs
@@ -12,7 +12,7 @@ namespace Host
     {
         static void Main(string[] args)
         {
-            var result = new IntersectionOfTwoLinkedLists().GetIntersectionNode(new ListNode(4, new ListNode(1, new ListNode(8, new ListNode(4, new ListNode(5))))), new ListNode(5, new ListNode(6, new ListNode(1, new ListNode(8, new ListNode(4, new ListNode(5)))))));
+            var result = new MinimumCostToMoveChipsToTheSamePosition().MinCostToMoveChips(new int[] { 6, 4, 7, 8, 2, 10, 2, 7, 9, 7 });
         }
     }
 }
