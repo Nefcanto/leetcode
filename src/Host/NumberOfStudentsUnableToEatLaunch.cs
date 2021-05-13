64423d0bd18cc1a91a35f81b5c83c950ce06afad
Nefcanto <sn.1361@gmail.com>
2021-05-13 13:32:52 +0430

    number of students unable to eat launch

---
+++ src/Host/NumberOfStudentsUnableToEatLaunch.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class NumberOfStudentsUnableToEatLaunch
+{
+    public int CountStudents(int[] students, int[] sandwiches)
+    {
+        var queue = new Queue();
+        var stack = new Stack();
+        var dic = new Dictionary<int, int>();
+        dic.Add(0, 0);
+        dic.Add(1, 0);
+        for (int i = sandwiches.Length - 1; i >= 0; i--)
+        {
+            stack.Push(sandwiches[i]);
+        }
+        for (int i = 0; i < students.Length; i++)
+        {
+            queue.Enqueue(students[i]);
+            dic[students[i]]++;
+        }
+        do
+        {
+            var sandwich = (int)stack.Peek();
+            var preference = (int)queue.Peek();
+            if (preference == sandwich)
+            {
+                stack.Pop();
+                queue.Dequeue();
+                dic[preference]--;
+            }
+            else
+            {
+                queue.Dequeue();
+                queue.Enqueue(preference);
+                if (dic[sandwich] == 0)
+                {
+                    break;
+                }
+            }
+        }
+        while (stack.Count > 0);
+        return queue.Count;
+    }
+}
