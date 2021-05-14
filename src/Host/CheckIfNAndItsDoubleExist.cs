7e2988a2db5ebed9939f2c3e49e9f46c21bdacc6
Nefcanto <sn.1361@gmail.com>
2021-05-14 11:40:55 +0430

    check if N and its double exist

---
+++ src/Host/CheckIfNAndItsDoubleExist.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+public class CheckIfNAndItsDoubleExist
+{
+    public bool CheckIfExist(int[] arr)
+    {
+        var search = new Dictionary<int, int>();
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (!search.ContainsKey(arr[i]))
+            {
+                search.Add(arr[i], i);
+            }
+        }
+        for (int i = 0; i < arr.Length; i++)
+        {
+            var @double = arr[i] * 2;
+            if (search.ContainsKey(@double) && search[@double] != i)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
