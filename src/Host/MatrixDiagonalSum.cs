04a63b83ee0b2ea2e7e21ccf33dcf6d09af5c0a9
Nefcanto <sn.1361@gmail.com>
2021-05-14 11:25:48 +0430

    diagonal sum

---
+++ src/Host/MatrixDiagonalSum.cs
@@ -0,0 +1,20 @@
+﻿public class MatrixDiagonalSum
+{
+    public int DiagonalSum(int[][] mat)
+    {
+        var sum = 0;
+        for (int i = 0; i < mat.Length; i++)
+        {
+            if (i == mat.Length - i - 1)
+            {
+                sum += mat[i][i];
+            }
+            else
+            {
+                sum += mat[i][i];
+                sum += mat[i][mat.Length - 1 - i];
+            }
+        }
+        return sum;
+    }
+}
