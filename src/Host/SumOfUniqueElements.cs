d9a6ec623463a0d124d947cfc64f09f889bf032b
Nefcanto <sn.1361@gmail.com>
2021-05-18 20:38:19 +0430

    sum of unique elements

---
+++ src/Host/SumOfUniqueElements.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int SumOfUnique(int[] nums) {
+        var uniques = new HashSet<int>();
+        var duplicates = new HashSet<int>();
+        for (int i = 0; i < nums.Length; i++) {
+            if (uniques.Contains(nums[i])) {
+                duplicates.Add(nums[i]);
+            }
+            else {
+                uniques.Add(nums[i]);
+            }
+        }
+        return uniques.Sum() - duplicates.Sum();
+    }
+}
