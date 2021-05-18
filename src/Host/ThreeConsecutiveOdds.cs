59e417fc22712272d147d41ade693d0f1fc6da2b
Nefcanto <sn.1361@gmail.com>
2021-05-18 20:48:30 +0430

    three consecutive odds

---
+++ src/Host/ThreeConsecutiveOdds.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public bool ThreeConsecutiveOdds(int[] arr) {
+        if (arr.Length < 3) {
+            return false;
+        }
+        for (int i = 2; i < arr.Length; i++) {
+            var threeOdds = arr[i] % 2 == 1 && arr[i-1] % 2 == 1 && arr[i-2] % 2 == 1;
+            if(threeOdds) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
