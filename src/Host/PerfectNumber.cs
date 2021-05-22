e0ce6ac79337e941c16e970265bf9ad5753edcca
Saeed Nemati <sn.1361@gmail.com>
2021-05-22 14:58:17 +0430

    Perfect Number

---
+++ src/Host/PerfectNumber.cs
@@ -0,0 +1,26 @@
+public class PerfectNumber.cs {
+    public bool CheckPerfectNumber(int num) {
+        if (num == 1) {
+            return false;
+        }
+        // find the positive divisers list
+        var divisors = new Dictionary<int, int>();
+        divisors.Add(1, 0);
+        var root = Math.Sqrt(num);
+        var x = 2;
+        while (x < root) {
+            if (num % x == 0) {
+                divisors.Add(x, num / x);
+            }
+            x++;
+        }
+        if (x * x == num) {
+            divisors.Add(x, 0);
+        }
+        var sum = divisors.Keys.Sum() + divisors.Values.Sum();
+        if (sum == num) {
+            return true;
+        }
+        return false;
+    }
+}
