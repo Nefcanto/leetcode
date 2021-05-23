0faad96d98c2b83c76245a26ae28ba6e53a43b7b
Saeed Nemati <sn.1361@gmail.com>
2021-05-23 20:56:53 +0430

    min stack

---
+++ src/Host/MinStack.cs
@@ -0,0 +1,39 @@
+public class MinStack
+{
+    int? min = null;
+    List<int> numbers = new List<int>();
+
+    /** initialize your data structure here. */
+    public MinStack()
+    {
+
+    }
+
+    public void Push(int val)
+    {
+        if (min == null)
+        {
+            min = val;
+        }
+        if (val < min)
+        {
+            min = val;
+        }
+        numbers.Add(val);
+    }
+
+    public void Pop()
+    {
+        numbers.RemoveAt(numbers.Count - 1);
+    }
+
+    public int Top()
+    {
+        return numbers.Last();
+    }
+
+    public int GetMin()
+    {
+        return numbers.Min();
+    }
+}
