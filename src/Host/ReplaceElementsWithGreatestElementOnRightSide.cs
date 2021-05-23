1544eb6298fd21200fad0f146d31f19ea2d631aa
Saeed Nemati <sn.1361@gmail.com>
2021-05-23 20:15:59 +0430

    replace elements with greatest element on right side

---
+++ src/Host/ReplaceElementsWithGreatestElementOnRightSide.cs
@@ -0,0 +1,16 @@
+public class Solution
+{
+    public int[] ReplaceElements(int[] arr)
+    {
+        var max = arr[arr.Length - 1];
+        var old = arr[arr.Length - 1];
+        for (int i = arr.Length - 2; i >= 0; i--)
+        {
+            max = Math.Max(arr[i + 1], old);
+            old = arr[i];
+            arr[i] = max;
+        }
+        arr[arr.Length - 1] = -1;
+        return arr;
+    }
+}
