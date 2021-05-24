71d73f29a878f67eab28b8feb06bd22319771903
Saeed Nemati <sn.1361@gmail.com>
2021-05-24 20:56:28 +0430

    rotate string

---
+++ src/Host/RotateString.cs
@@ -0,0 +1,25 @@
+public class Solution
+{
+    public bool RotateString(string s, string goal)
+    {
+        if (string.IsNullOrEmpty(s))
+        {
+            return string.IsNullOrEmpty(goal);
+        }
+        if (s.Length != goal.Length)
+        {
+            return false;
+        }
+        var count = 0;
+        while (count < s.Length)
+        {
+            s = s.Substring(1) + s[0].ToString();
+            if (s == goal)
+            {
+                return true;
+            }
+            count++;
+        }
+        return false;
+    }
+}
