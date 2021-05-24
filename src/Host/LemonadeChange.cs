9bf08a6216d4ed1b25c3b27652f18f803b9f61a7
Saeed Nemati <sn.1361@gmail.com>
2021-05-24 21:56:18 +0430

    lemonade change

---
+++ src/Host/LemonadeChange.cs
@@ -0,0 +1,63 @@
+// not working either
+public class Solution
+{
+    public bool LemonadeChange(int[] bills)
+    {
+        var sum = 0;
+        for (int i = 0; i < bills.Length; i++)
+        {
+            sum += bills[i];
+            sum -= (bills[i] - 5);
+            if (sum < 0 || sum % 10 == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+// not working
+public class Solution
+{
+    public bool LemonadeChange(int[] bills)
+    {
+        var fives = 0;
+        var tens = 0;
+        var twenties = 0;
+        for (int i = 0; i < bills.Length; i++)
+        {
+            if (bills[i] != 5 && fives == 0)
+            {
+                return false;
+            }
+            if (bills[i] == 5)
+            {
+                fives++;
+            }
+            else if (bills[i] == 10)
+            {
+                tens++;
+                fives--;
+            }
+            else
+            {
+                if (tens == 0 && fives < 3)
+                {
+                    return false;
+                }
+                twenties++;
+                if (tens > 1)
+                {
+                    tens--;
+                    fives--;
+                }
+                else
+                {
+                    fives -= 3;
+                }
+            }
+        }
+        return true;
+    }
+}
