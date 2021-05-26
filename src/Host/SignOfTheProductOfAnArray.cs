77f95ebbca0af9c5fbc341e3db4a654560819f21
Saeed Nemati <sn.1361@gmail.com>
2021-05-26 12:57:36 +0430

    sign of the product of an array

---
+++ src/Host/SignOfTheProductOfAnArray.cs
@@ -0,0 +1,19 @@
+public class Solution
+{
+    public int ArraySign(int[] nums)
+    {
+        var negatives = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == 0)
+            {
+                return 0;
+            }
+            if (nums[i] < 0)
+            {
+                negatives++;
+            }
+        }
+        return negatives % 2 == 0 ? 1 : -1;
+    }
+}
