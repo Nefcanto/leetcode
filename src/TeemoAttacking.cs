20218b0d4ef621ea8414ce601413317a2935dcd5
Saeed Nemati <sn.1361@gmail.com>
2021-05-29 22:09:00 +0430

    making changes to run on linux

---
+++ src/TeemoAttacking.cs
@@ -0,0 +1,13 @@
+public class Solution
+{
+    public int FindPoisonedDuration(int[] timeSeries, int duration)
+    {
+        var sum = 0;
+        for (int i = 0; i < timeSeries.Length - 1; i++)
+        {
+            var min = Math.Min(timeSeries[i] + duration - 1, timeSeries[i + 1] - 1);
+            sum += (min - timeSeries[i]) + 1;
+        }
+        return sum + duration;
+    }
+}
