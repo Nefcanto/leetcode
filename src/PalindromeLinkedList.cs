fa768f3f379ae7a544333b12f79b95c1748f8df2
Saeed Nemati <sn.1361@gmail.com>
2021-05-31 11:02:54 +0430

    palindrome linked list

---
+++ src/PalindromeLinkedList.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+
+public class Solution
+{
+    public bool IsPalindrome(ListNode head)
+    {
+        var values = new List<int>();
+        while (head != null)
+        {
+            values.Add(head.val);
+            head = head.next;
+        }
+        for (int i = 0, j = values.Count - 1; i <= j; i++, j--)
+        {
+            if (values[i] != values[j])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
