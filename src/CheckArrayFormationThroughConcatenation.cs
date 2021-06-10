5c2b6aa86f80e0f42c890d2c002d7bfbe7bb846c
Saeed Nemati <sn.1361@gmail.com>
2021-06-10 16:56:48 +0430

    comparing java and c#

--- src/CheckArrayFormationThroughConcatenation.cs
+++ src/CheckArrayFormationThroughConcatenation.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
+using System.Text;
 
-public class CheckArrayFormationThroughConcatenation
+public class Solution
 {
     public bool CanFormArray(int[] arr, int[][] pieces)
     {
@@ -31,4 +32,56 @@ public class CheckArrayFormationThroughConcatenation
         }
         return true;
     }
+    
+    // compare this with Java version at the end. Java 2ms, this 100ms.
+    public bool CanFormArray2(int[] arr, int[][] pieces)
+    {
+        StringBuilder sb = new StringBuilder();
+        foreach (int i in arr)
+        {
+            sb.Append("#");
+            sb.Append(i);
+            sb.Append("#");
+        }
+        for (int i = 0; i < pieces.Length; i++)
+        {
+            StringBuilder res = new StringBuilder();
+            for (int j = 0; j < pieces[i].Length; j++)
+            {
+                res.Append("#");
+                res.Append(pieces[i][j]);
+                res.Append("#");
+            }
+            if (!sb.ToString().Contains(res.ToString()))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 }
+
+/*
+class Solution {
+    public boolean canFormArray(int[] arr, int[][] pieces) {
+        StringBuilder sb = new StringBuilder();
+        for(int x : arr){
+			sb.append("#");
+            sb.append(x);
+            sb.append("#");
+        }
+        for(int i = 0; i < pieces.length; i++){
+            StringBuilder res = new StringBuilder();
+            for(int j = 0; j < pieces[i].length; j++){
+				res.append("#");
+                res.append(pieces[i][j]);
+                res.append("#");
+            }
+            if(!sb.toString().contains(res.toString())){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+*/
